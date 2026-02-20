cb3f07f582e118892938bbd17433d7e1d3441161
asxxfaq <abdulashfaqehere@gmail.com>
2026-02-20 12:50:24 +0530

    Zee Arts core

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,59 +1,37 @@
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaHome, FaInstagram } from "react-icons/fa";
-import { LuMenu, LuX, LuShoppingCart, LuHeart } from "react-icons/lu";
+import { LuShoppingCart, LuHeart } from "react-icons/lu";
 import { GrGallery } from "react-icons/gr";
 import { useStore } from "../context/StoreContext";
-import logo from "../src/images/logo.jpg";   // ✅ IMPORT IMAGE
 import "./Navbar.css";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
   const { cart, favourites } = useStore();
 
   return (
-    <nav className="navbar">
+    <div className="floating-nav-container">
+      <nav className="pill-navbar">
+        
+        {/* HOME */}
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}
+        >
+          <FaHome size={24} />
+        </NavLink>
 
-      {/* BRAND */}
-      <div className="nav-brand">
-        <img
-          src={logo}
-          alt="ZeeArtsCore Logo"
-          className="logo"
-        />
-        <div>
-          <h2>ZeeArtsCore</h2>
-          <small>Bouquet Birthday Gifts</small>
-        </div>
-      </div>
-
-      {/* HAMBURGER */}
-      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? <LuX size={28} /> : <LuMenu size={28} />}
-      </div>
-
-      {/* NAV CONTENT */}
-      <div className={`nav-actions ${isOpen ? "show" : ""}`}>
-
-        {/* LINKS */}
-        <ul className="nav-links">
-          <li>
-            <NavLink to="/" onClick={() => setIsOpen(false)}>
-              <FaHome size={20} />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/gallery" onClick={() => setIsOpen(false)}>
-              <GrGallery size={20} />
-            </NavLink>
-          </li>
-        </ul>
+        {/* GALLERY */}
+        <NavLink 
+          to="/gallery" 
+          className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}
+        >
+          <GrGallery size={22} />
+        </NavLink>
 
         {/* ❤️ FAVOURITES */}
-        <NavLink
-          to="/favourites"
-          className="nav-icon-wrapper"
-          onClick={() => setIsOpen(false)}
+        <NavLink 
+          to="/favourites" 
+          className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}
         >
           <LuHeart size={24} />
           {favourites.length > 0 && (
@@ -62,10 +40,9 @@ function Navbar() {
         </NavLink>
 
         {/* 🛒 CART */}
-        <NavLink
-          to="/cart"
-          className="nav-icon-wrapper"
-          onClick={() => setIsOpen(false)}
+        <NavLink 
+          to="/cart" 
+          className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}
         >
           <LuShoppingCart size={24} />
           {cart.length > 0 && (
@@ -73,18 +50,18 @@ function Navbar() {
           )}
         </NavLink>
 
-        {/* WHATSAPP */}
+        {/* WHATSAPP / INSTAGRAM */}
         <a
           href="https://ig.me/m/zeeartscore"
           target="_blank"
           rel="noreferrer"
-          className="instagram-btn"
+          className="nav-item"
         >
           <FaInstagram size={24} />
-          <span>Order Now</span> {/* Optional text, you can remove the span if you only want the icon */}
         </a>
-      </div>
-    </nav>
+
+      </nav>
+    </div>
   );
 }
 
